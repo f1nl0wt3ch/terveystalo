f32cbc59537be3bee7f0b83ac7a229c5fc627fd3
Thinh Dinh <thinh.dinh@aktia.fi>
2021-04-05 21:36:48 +0300

    Created database connection and restructure project

---
+++ server/tsc/config/DatabaseConfig.ts
@@ -0,0 +1,7 @@
+
+export abstract class DatabaseConfig {
+    static readonly  HOST: string = "sql4.freemysqlhosting.net"
+    static readonly USER: string = "sql4402980"
+    static readonly PASSWORD: string = "Dq1rR9yqER"
+    static readonly DATABASE: string = "sql4402980"
+}
